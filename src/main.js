3a961eb7a8dbd4d3440f1651da76280ec77691dc
范志鹏2002 <2710557309@qq.com>
2023-12-31 00:19:01 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入字体图标
+import './styles/index.css'
+
+// 导入vant相关的样式和组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 注册vant组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
